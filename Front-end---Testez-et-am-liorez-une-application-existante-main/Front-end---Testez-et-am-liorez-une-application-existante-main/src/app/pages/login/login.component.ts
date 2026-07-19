fa5741104017fd8375c98d9727fde3fbe9ba3cc2
David Duribreux <duribreuxdavid15@gmail.com>
2026-07-19 22:16:20 +0200

    feat(auth): ajouter l'interface de connexion Angular

---
+++ Front-end---Testez-et-am-liorez-une-application-existante-main/Front-end---Testez-et-am-liorez-une-application-existante-main/src/app/pages/login/login.component.ts
@@ -0,0 +1,63 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../shared/material.module';
+import { UserService } from '../../core/service/user.service';
+import { Login } from '../../core/models/Login';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, MaterialModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  private userService = inject(UserService);
+
+  loginForm: FormGroup = new FormGroup({});
+  submitted: boolean = false;
+  successMessage: string = '';
+  errorMessage: string = '';
+  loading: boolean = false;
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get form() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    this.successMessage = '';
+    this.errorMessage = '';
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const loginUser: Login = {
+      login: this.loginForm.get('login')?.value,
+      password: this.loginForm.get('password')?.value
+    };
+
+    this.loading = true;
+
+    this.userService.login(loginUser).subscribe({
+      next: (token: string) => {
+        this.loading = false;
+        sessionStorage.setItem('authToken', token);
+        this.successMessage = 'Connexion réussie.';
+      },
+      error: () => {
+        this.loading = false;
+        this.errorMessage = 'Login ou mot de passe incorrect.';
+      }
+    });
+  }
+}
